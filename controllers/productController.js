91a82d265bfaac3f1954fc60be26fdf5313f347c
saipriyagudipati13-sudo <saipriyagudipati13@gmail.com>
2026-03-10 13:40:20 +0530

    updated layout footer

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,7 +12,32 @@ const addProduct = async(req, res) => {
 };
 
 const addProductForm = async(req, res) => {
-    res.render("products/add")
-}
+    res.render("products/add");
+};
 
-export { getProducts, addProduct, addProductForm };
+const deleteProduct = async(req, res) => {
+    const id = req.params.id;
+    await productModel.findByIdAndDelete(id);
+    res.redirect("/products");
+};
+
+const editProductForm = async(req, res) => {
+    const id = req.params.id;
+    const product = await productModel.findOne({ _id: id });
+    res.render("products/edit", { product });
+};
+
+const saveProduct = async(req, res) => {
+    const id = req.params.id;
+    await productModel.findByIdAndUpdate(id, req.body);
+    res.redirect("/products")
+};
+
+export {
+    getProducts,
+    addProduct,
+    addProductForm,
+    deleteProduct,
+    editProductForm,
+    saveProduct
+};
